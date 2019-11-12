f32636492d8ff20bc5f1a344d97d34c9d468e16f
Jacob Swanson <jswan12@lsu.edu>
2019-11-12 02:00:38 -0600

    add support for changing verified status in the database

--- Impact/Impact/EmailVerificationPage.xaml.cs
+++ Impact/Impact/EmailVerificationPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +21,8 @@ namespace Impact
             InitializeComponent();
             email = new EmailSender(recipientEmail, true);
             recipientEmailLabel.Text = recipientEmail;
+            NavigationPage.SetHasBackButton(this, true);
+            verificationCodeEntry.ReturnCommand = new Command(() => submitButton.Focus());
         }
 
         private async void SubmitButtonClicked(object sender, EventArgs e)
@@ -26,13 +31,39 @@ namespace Impact
               then verify user's account and send to the homescreen.*/
             if(!string.IsNullOrWhiteSpace(verificationCodeEntry.Text) && string.Equals(email.getVerificationCode(), verificationCodeEntry.Text))
             {
-                await DisplayAlert("Success", "Verification Successful", "OK");
                 email = null;
+                try
+                {
+                    activityIndicator.IsRunning = true;
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        HttpResponseMessage response = await client.PutAsync(new Uri("https://asp-impact.azurewebsites.net/api/Login"), new StringContent(App.currentUser.credentials_id.ToString(), Encoding.UTF8, "application/json"));
+                        var responseBody = response.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
+                        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+                        {
+                            // Email Set to Verified.
+                            await DisplayAlert("Success", "Email Verification Successful", "OK");
 
-                //TODO
-                //Need to post to the database that the user is verified.
+                            //TODO - NEED TO CHECK IF ACCOUNT REGISTRATION HAS BEEN COMPLETED
 
-                Application.Current.MainPage = new TabMainPage();
+
+                            Application.Current.MainPage = new TabMainPage();
+                        }
+                        //Couldnt Find Credentials with credentials_id provided of the current user
+                        //This should never happen!!!!
+                        else
+                            await DisplayAlert("Error", responseBody, "OK");
+                    }
+                }
+                catch (Exception exception)
+                {
+                    await DisplayAlert("Error", exception.Message, "OK");
+                }
+                finally
+                {
+                    activityIndicator.IsRunning = false;
+                }
             }
             else
                 await DisplayAlert("Error", "Invalid Verification Code", "OK");
