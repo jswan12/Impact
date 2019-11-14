f701538684fdb67c3523de3c678c02cfe078028a
Clauida Caldera <ccalde4@lsu.edu>
2019-11-14 16:35:33 -0600

    Start registration page

---
+++ Impact/Impact/SignUp.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Impact
+{
+    public partial class SignUp : ContentView
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
