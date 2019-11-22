f97c08fa293bda91fdfbc1f830a63f0f1719fda6
Jacob Swanson <jswan12@lsu.edu>
2019-11-21 20:22:33 -0600

    add Groups Page

---
+++ Impact/Impact/GroupsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Impact;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Impact
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GroupsPage : ContentPage
+    {
+        public GroupsPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
