7a5e91185e6249a86b5f25989b26cb27dbf9c00d
Jacob Swanson <jswan12@lsu.edu>
2019-11-24 15:14:55 -0600

    refactor update user's verification status

--- Impact/Impact/EmailVerificationPage.xaml.cs
+++ Impact/Impact/EmailVerificationPage.xaml.cs
@@ -32,41 +32,46 @@ namespace Impact
             if(!string.IsNullOrWhiteSpace(verificationCodeEntry.Text) && string.Equals(email.getVerificationCode(), verificationCodeEntry.Text))
             {
                 email = null;
-                try
-                {
-                    activityIndicator.IsRunning = true;
-                    using (HttpClient client = new HttpClient())
-                    {
-                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        HttpResponseMessage response = await client.PutAsync(new Uri("https://asp-impact.azurewebsites.net/api/Login"), new StringContent(App.currentUser.credentials_id.ToString(), Encoding.UTF8, "application/json"));
-                        var responseBody = response.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
-                        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
-                        {
-                            // Email Set to Verified.
-                            await DisplayAlert("Success", "Email Verification Successful", "OK");
-
-                            if (string.IsNullOrEmpty(App.currentUser.name))
-                                App.instance.ClearNavigationAndGoToPage(new RegistrationPage());
-                            else
-                                App.instance.ClearNavigationAndGoToPage(new TabMainPage());
-                        }
-                        //Couldnt Find Credentials with credentials_id provided of the current user
-                        //This should never happen with the database foreign key reference !!!!
-                        else
-                            await DisplayAlert("Error", responseBody, "OK");
-                    }
-                }
-                catch (Exception exception)
-                {
-                    await DisplayAlert("Error", exception.Message, "OK");
-                }
-                finally
-                {
-                    activityIndicator.IsRunning = false;
-                }
+                await updateUsersVerificationStatus();
             }
             else
                 await DisplayAlert("Error", "Invalid Verification Code", "OK");
+        }
+
+        private async Task updateUsersVerificationStatus()
+        {
+            try
+            {
+                activityIndicator.IsRunning = true;
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.PutAsync(new Uri("https://asp-impact.azurewebsites.net/api/Login"), new StringContent(App.currentUser.credentials_id.ToString(), Encoding.UTF8, "application/json"));
+                    var responseBody = response.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
+                    if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        // Email Set to Verified.
+                        await DisplayAlert("Success", "Email Verification Successful", "OK");
+
+                        if (string.IsNullOrEmpty(App.currentUser.name))
+                            App.instance.ClearNavigationAndGoToPage(new RegistrationPage());
+                        else
+                            App.instance.ClearNavigationAndGoToPage(new TabMainPage());
+                    }
+                    //Couldnt Find Credentials with credentials_id provided of the current user
+                    //This should never happen with the database foreign key reference !!!!
+                    else
+                        await DisplayAlert("Error", responseBody, "OK");
+                }
+            }
+            catch (Exception exception)
+            {
+                await DisplayAlert("Error", exception.Message, "OK");
+            }
+            finally
+            {
+                activityIndicator.IsRunning = false;
+            }
         }
     }
 }
