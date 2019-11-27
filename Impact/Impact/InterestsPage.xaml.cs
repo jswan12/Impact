b96ae8aec54e3f9989f0d93966f90c8c9ff0fa44
Jacob Swanson <jswan12@lsu.edu>
2019-11-27 00:47:20 -0600

    add saving user info functionality

--- Impact/Impact/InterestsPage.xaml.cs
+++ Impact/Impact/InterestsPage.xaml.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Impact
 {
     public partial class InterestsPage : ContentPage
     {
-        public InterestsPage()
+        User newUserInfo;
+        public InterestsPage(User newUserInfo)
         {
             InitializeComponent();
-
+            this.newUserInfo = newUserInfo;
             var interestsList = new List<string>();
             interestsList.Add("Acting or Entertaining");
             interestsList.Add("Communication");
@@ -37,12 +39,52 @@ namespace Impact
 
             var hobbiesPicker = new Picker { Title = "Select a Hobby", TitleColor = Color.Red };
             hobbiesPicker.ItemsSource = hobbiesList;
-
         }
 
-        private void finishPage_ButtonClicked(object sender, EventArgs e)
+        private async void finishPage_ButtonClicked(object sender, EventArgs e)
         {
-            App.instance.ClearNavigationAndGoToPage(new TabMainPage());
+            newUserInfo.interest1 = interests1Picker.SelectedItem.ToString();
+            newUserInfo.interest2 = interests2Picker.SelectedItem.ToString();
+            newUserInfo.interest3 = interests3Picker.SelectedItem.ToString();
+            newUserInfo.hobby1 = hobbies1Picker.SelectedItem.ToString();
+            newUserInfo.hobby2 = hobbies2Picker.SelectedItem.ToString();
+            newUserInfo.hobby3 = hobbies3Picker.SelectedItem.ToString();
+
+            await createUserInfoAsync();
+        }
+
+        private async Task createUserInfoAsync()
+        {
+            try
+            {
+                //activityIndicator.IsRunning = true;
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var newUserInfo_JSON = new StringContent(JsonConvert.SerializeObject(newUserInfo), Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PutAsync(new Uri("https://asp-impact.azurewebsites.net/api/User"), newUserInfo_JSON);
+                    string responseBody = response.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        var deserializationSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, MissingMemberHandling = MissingMemberHandling.Ignore };
+                        App.currentUser = JsonConvert.DeserializeObject<User>(responseBody, deserializationSettings);
+
+                        //User Info Successfully added
+                        App.instance.ClearNavigationAndGoToPage(new TabMainPage());
+                    }
+                    else
+                        await DisplayAlert("Error", responseBody, "OK");
+                }
+            }
+            catch (Exception exception)
+            {
+                await DisplayAlert("Error", exception.Message, "OK");
+            }
+            finally
+            {
+                //activityIndicator.IsRunning = false;
+            }
         }
     }
 }
--- Impact/Impact/ProfilePage.xaml.cs
+++ Impact/Impact/ProfilePage.xaml.cs
@@ -24,6 +24,13 @@ namespace Impact
             profilePage_state.Text = "State: " + App.currentUser.state;
             profilePage_gender.Text = "Gender: " + App.currentUser.gender;
             profilePage_major.Text = "Major: " + App.currentUser.major;
+            profilePage_userType.Text = "user_type: " + App.currentUser.user_type;
+            profilePage_interest1.Text = "interest1: " + App.currentUser.interest1;
+            profilePage_interest2.Text = "interest2: " + App.currentUser.interest2;
+            profilePage_interest3.Text = "interest3: " + App.currentUser.interest3;
+            profilePage_hobby1.Text = "hobby1: " + App.currentUser.hobby1;
+            profilePage_hobby2.Text = "hobby2: " + App.currentUser.hobby2;
+            profilePage_hobby3.Text = "hobby3: " + App.currentUser.hobby3;
         }
 
 /*        protected override async void OnAppearing()
