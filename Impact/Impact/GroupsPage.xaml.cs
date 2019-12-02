30190d81b82019724ae9234b6cf375aeebaba534
sarah <sbroom7@lsu.edu>
2019-12-01 23:11:13 -0600

    update to registration page, add new groups page

--- Impact/Impact/GroupsPage.xaml.cs
+++ Impact/Impact/GroupsPage.xaml.cs
@@ -17,6 +17,34 @@ namespace Impact
             InitializeComponent();
         }
 
-    }
+        private void mainGroup1_ButtonClicked(object sender, EventArgs e)
+        {
+            App.instance.ClearNavigationAndGoToPage(new MainGroup());
+        }
 
+        async private void mainGroup2_ButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+
+        async private void mainGroup3_ButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+
+        async private void subGroupA_ButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+
+        async private void subGroupB_ButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+
+        async private void subGroupC_ButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+    }
 }
