0c0b70ab0815eb8f472168597d0bd3e8740f999b
Jacob Swanson <jswan12@lsu.edu>
2019-12-01 21:53:18 -0600

    add User bio attribute

--- Impact/Impact/User.cs
+++ Impact/Impact/User.cs
@@ -24,5 +24,6 @@ namespace Impact
         public string hobby1 { get; set; }
         public string hobby2 { get; set; }
         public string hobby3 { get; set; }
+        public string bio { get; set; }
     }
 }
